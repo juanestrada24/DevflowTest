bd4e71e3465d53cc0704d2a86ff3b687ced9046f
juanestrada24 <juanestrada24@gmail.com>
2025-06-14 23:15:44 -0400

    Update 2_📊_Ficha_Analisis.py

--- pages/2_📊_Ficha_Analisis.py
+++ pages/2_📊_Ficha_Analisis.py
@@ -83,7 +83,7 @@ st.markdown(
 )
 
 # --- KPIs Principales en grid de 3 columnas con fuente más pequeña y ROI en verde limón ---
-st.title("2_Analisis.py")
+st.title("Analysis.py")
 
 st.markdown(
     """
